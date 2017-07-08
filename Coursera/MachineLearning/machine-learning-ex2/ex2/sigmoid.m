29fc410ed6c4a146ba9a45822a3e20b5a1248049
Kris_Chan <kris.dacpc@gmail.com>
2017-07-09 00:03:26 +0800

    1.finish the sigmoid function;

--- Coursera/MachineLearning/machine-learning-ex2/ex2/sigmoid.m
+++ Coursera/MachineLearning/machine-learning-ex2/ex2/sigmoid.m
@@ -8,11 +8,11 @@ g = zeros(size(z));
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
-
-
-
-
-
 % =============================================================
-
+[row,col] = size(z);
+for i = 1:row
+    for j = 1:col
+        g(i,j) = 1 / (1 + exp(-z(i,j)));
+    end
+end
 end
