a685efcabed1eed2f2ad6889e424372331af547e
Kris_Chan <kris.dacpc@gmail.com>
2017-07-11 18:14:08 +0800

    1.finish the costfunction(3);

--- Coursera/MachineLearning/machine-learning-ex3/ex3/lrCostFunction.m
+++ Coursera/MachineLearning/machine-learning-ex3/ex3/lrCostFunction.m
@@ -35,18 +35,14 @@ grad = zeros(size(theta));
 %           temp(1) = 0;   % because we don't add anything for j = 0  
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
-
-
-
-
-
-
-
-
-
-
 % =============================================================
 
 grad = grad(:);
+
+h = sigmoid(X * theta);
+J = (sum((-y) .* log(h) - (1 - y) .* log(1 - h))) * (1 / m) + (lambda / (2 * m)) * (sum(theta(2:end).^2));
+
+grad(1) = (1 / m) * sum((h - y)' * X(:,1));
+grad(2:end) = ((1 / m) * (h - y)' * X(:,2:end))' + (lambda / m) * theta(2:end);
 
 end
