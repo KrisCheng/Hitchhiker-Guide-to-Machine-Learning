527c7743d3d2443dd24c682f0912ffd037572d91
Kris_Chan <kris.dacpc@gmail.com>
2017-10-25 10:35:33 +0800

    1.add a linera mode;

---
+++ Model/linear_unit.py
@@ -0,0 +1,31 @@
+#!~/anaconda/bin/python
+
+__author__ = "Kris Peng"
+
+# a implement of linear unit, based on Python2.7
+from perceptron import Perceptron
+
+f = lambda x: x
+
+class LinearUnit(Perceptron):
+    def __init__(self, input_num):
+        Perceptron.__init__(self, input_num, f)
+
+def get_training_dataset():
+    input_vecs = [[5], [3], [8], [1.4], [10.1]]
+    label = [5500, 2300, 7600, 1800, 11400]
+    return input_vecs, label
+
+def train_linear_unit():
+    lu = LinearUnit(1)
+    input_vecs, labels = get_training_dataset()
+    lu.train(input_vecs, labels, 10, 0.01)
+    return lu
+
+if __name__ == '__main__':
+    linear_unit = train_linear_unit()
+    print(linear_unit)
+    print("Work 3.4 years, monthly salary = %.2f'" % linear_unit.predict([3.4]))
+    print("Work 15 years, monthly salary = %.2f'" % linear_unit.predict([15]))
+    print("Work 6 years, monthly salary = %.2f'" % linear_unit.predict([6]))
+    print("Work 27 years, monthly salary = %.2f'" % linear_unit.predict([27]))
--- Model/perceptron.py
+++ Model/perceptron.py
@@ -21,8 +21,8 @@ class Perceptron(object):
         return self.activator(
             reduce(lambda a, b: a + b,
                    map(lambda x_w: x_w[0] * x_w[1],
-                   # map(lambda (x, w): x * w,
-                       zip(input_vec, self.weights))
+                   # map(lambda (x, w): x * w, # Only in Python2
+                       list(zip(input_vec, self.weights)))
                    , 0.0) + self.bias)
 
     def train(self, input_vecs, labels, iteration, rate):
@@ -30,17 +30,18 @@ class Perceptron(object):
             self._one_iteration(input_vecs, labels, rate)
 
     def _one_iteration(self, input_vecs, labels, rate):
-        samples = zip(input_vecs, labels)
+        samples = list(zip(input_vecs, labels))
         for(input_vec, label) in samples:
             output = self.predict(input_vec)
             self._update_weights(input_vec, output, label, rate)
 
     def _update_weights(self, input_vec, output, label, rate):
         delta = label - output
+
         self.weights = map(
             lambda x_w: x_w[1] + rate * delta * x_w[0],
             # lambda (x, w): w + rate * delta * x,
-            zip(input_vec, self.weights))
+            list(zip(input_vec, self.weights)))
 
         self.bias += rate * delta
 
@@ -48,6 +49,7 @@ def f(x):
     return 1 if x > 0 else 0
 
 def get_training_dataset():
+    # and training set
     input_vecs = [[1, 1], [0, 0], [1, 0], [0, 1]]
     labels = [1, 0, 0, 0]
     return input_vecs, labels
@@ -55,7 +57,7 @@ def get_training_dataset():
 def train_and_perceptron():
     p = Perceptron(2, f)
     input_vecs, labels = get_training_dataset()
-    p.train(input_vecs, labels, 10, 0.1)
+    p.train(input_vecs, labels, 10, 0.01)
     return p
 
 if __name__ == '__main__':
