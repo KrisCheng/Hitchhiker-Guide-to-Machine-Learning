ff53c8994d9d54f30fe2d7303329a05ebec31c9b
Kris_Chan <kris.dacpc@gmail.com>
2018-03-23 20:49:34 +0800

    1.add the Encoder-decoder model;

---
+++ Model/sequence/EncoderDecoderLSTM.py
@@ -0,0 +1,6 @@
+'''
+Desc: the Encoder-Decoder LSTM model for Sequence to Sequence Prediction Problem, based on keras.
+Author: Kris Peng
+Copyright (c) 2018 - Kris Peng <kris.dacpc@gmail.com>
+'''
+
